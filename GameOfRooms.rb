e162df44baf578c9a284459f1e595df96eac0e33
Juan <juan.antonadelval@gmail.com>
2015-04-07 19:44:55 +0200

    advanced_file

---
+++ GameOfRooms.rb
@@ -0,0 +1,97 @@
+require "pry"
+
+#########################
+#CLASS DECLARATION
+#########################
+
+class Room
+  
+  attr_accessor :id, :description, :options
+
+  def initialize(id, description)
+    @id = id
+    @description = description
+	@options = []
+  end
+
+  def add_option(direction, next_id)
+  	@options.push([direction, next_id])
+  end	
+
+end
+
+class Map
+  
+  attr_accessor :rooms
+
+  def initialize
+  	@rooms = []
+  end
+
+  def add_room(room)
+    @rooms = room
+  end	
+
+end
+
+class Game
+  
+  attr_accessor :current_position, :player_positions
+
+  def initialize
+  	@current_stage = "stage0"
+  	@current_position = 0
+  	@player_positions = []
+  end
+
+  def execute_command(command)
+  	@current_stage.rooms.each do |rooms|
+  		
+  		if @current_position == rooms.id
+
+  		end	
+
+  	
+
+  	@player_positions.push(command)
+  end 
+
+end	
+
+#########################
+#INSTANCE STAGE 1
+#########################
+
+room0 = Room.new 0, "text_room0"
+room0.add_options ["S", 1]
+room1 = Room.new 1, "text_room1"
+room1.add_options ["S", 2]
+room1.add_options ["W", 4]
+room2 = Room.new 2, "text_room2"
+room2.add_options ["W", 3]
+room3 = Room.new 3, "text_room3"
+room3.add_options ["N", 4]
+room3.add_options ["S", 5]
+room4 = Room.new 4, "text_room4"
+room4.add_options ["E", 1]
+room4.add_options ["S", 3]
+room5 = Room.new 5, "text_room5"
+
+stage0 = Map.new
+sateg0.add_room(room0)
+sateg0.add_room(room1)
+sateg0.add_room(room2)
+sateg0.add_room(room3)
+sateg0.add_room(room4)
+sateg0.add_room(room5)
+
+#########################
+#MAIN
+#########################
+
+juan = Game.new 
+
+puts "Where do you go?"
+command = gets.chomp
+
+juan.execute_command(command)
